d897af07e1dfa6b1c6ed95b8af0e032764090cd7
Stefan E-K <stefan.eideloth@gmail.com>
2026-09-16 21:35:34 +0200

    chore: remove unused Tailwind tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import solid from "@solidjs/vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
+  plugins: [solid()],
 });
